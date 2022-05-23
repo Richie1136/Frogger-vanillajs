34c54c155a005f09beba6b9f539b9941a0a5c3cb
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-23 11:47:22 -0400

    Grabbing all of the div inside of the grid class

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const timeLeft = document.querySelector('#time-left')
 const result = document.querySelector('#result')
+const startorpause = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
