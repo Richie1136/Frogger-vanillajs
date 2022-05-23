9580a3b0497ac8a3e3b7b3601f59446c6072baa2
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-23 11:53:48 -0400

    Added the frog class to the starting block

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const timeLeft = document.querySelector('#time-left')
 const result = document.querySelector('#result')
 const startorpause = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
+
+let currentIndex = 76
+
+const moveFrog = () => {
+  console.log("Moved")
+  squares[currentIndex].classList.add('frog')
+}
+
+document.addEventListener('keyup', moveFrog)
